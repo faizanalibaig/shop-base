ca53ecdbc7140fe562239005d357225149e231a2
faizanalibaig <riazfiaz6181@gmail.com>
2025-01-14 18:20:17 +0500

    title centered for test

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Shop - Base</h1>
+    <div className='w-screen h-screen overflow-hidden flex justify-center items-center'>
+      <h1 className='font-semibold text-2xl'>Shop - Base</h1>
     </div>
   );
 }
