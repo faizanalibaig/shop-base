647e598a0767d145a13b6ad98f182c89aaf256a5
faizanalibaig <riazfiaz6181@gmail.com>
2025-01-20 21:38:09 +0500

    dashboard setup start

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from '@/components/HeaderSection/Dashboard/LoginPage';
+
+function Dashboard() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ components/HeaderSection/Dashboard/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="font-newsreader">LoginPage</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
